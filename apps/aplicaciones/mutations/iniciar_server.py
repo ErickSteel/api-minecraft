16403df33106ee83433f77e60af5368d86a82f68
Erick <erickrg192@gmail.com>
2020-09-28 18:44:21 -0300

    cambios en las rutas para iniciar el server

--- apps/aplicaciones/mutations/iniciar_server.py
+++ apps/aplicaciones/mutations/iniciar_server.py
@@ -1,18 +1,33 @@
 import graphene
 import os
 
+
 class IniciarServer(graphene.Mutation):
 	estado = graphene.Boolean()
-	minecraft_dir = graphene.String()
-	nombre_jar = graphene.String()
-	class Arguments:
-		minecraft_dir = graphene.String(required=True)
-		nombre_jar = graphene.String(required=True)
 
-	def mutate(self, info, minecraft_dir, nombre_jar):
+	def mutate(self, info):
 		#'/home/erick/Escritorio/Server/'
-		direccion = '/'+minecraft_dir
+		direccion = '/home/erick/Escritorio/Server/'
+		nombre_jar = 'server-awa.jar'
 		comando = 'screen -dmS "minecraft" java -Xmx1024M -Xms1024M -jar '+nombre_jar+' --nogui'
+		
+		#os.chdir se mueve a una dirección espesifica
 		os.chdir(direccion)
 		os.system(comando)
-		return IniciarServer(estado=True, minecraft_dir=minecraft_dir, nombre_jar=nombre_jar)
+		return IniciarServer(estado=True)
+
+
+#class IniciarServer(graphene.Mutation):
+#	estado = graphene.Boolean()
+#	minecraft_dir = graphene.String()
+#	nombre_jar = graphene.String()
+#	class Arguments:
+#		minecraft_dir = graphene.String(required=True)
+#		nombre_jar = graphene.String(required=True)
+#
+#	def mutate(self, info, minecraft_dir, nombre_jar):
+#		direccion = minecraft_dir
+#		comando = 'screen -dmS "minecraft" java -Xmx1024M -Xms1024M -jar '+nombre_jar+' --nogui'
+#		os.chdir(direccion)
+#		os.system(comando)
+#		return IniciarServer(estado=True, minecraft_dir=minecraft_dir, nombre_jar=nombre_jar)
